eb322c533ccad1d4e08270f0cc238bfaeba57e91
Guigoun <guilhermelicati2004@gmail.com>
2026-03-01 19:20:58 -0300

    Implementando script de preenchimento de formulário com dados válidos

---
+++ cypress/e2e/Secao-Principal/Secao-Inscricao/inscricao-sucesso.cy.js
@@ -0,0 +1,22 @@
+describe('Preenchimento do formulário de inscrição', () => {
+
+  beforeEach(() => {
+    cy.visit('https://qualidade.apprbs.com.br/certificacao')
+  })
+
+  it('Deve preencher o formulário com nome válido', () => {
+
+    // Ignora o erro específico da aplicação
+    cy.on('uncaught:exception', (err) => {
+      if (err.message.includes('ActionsForm')) {
+        return false
+      }
+    })
+
+    // Preenche o campo Nome
+    cy.get('input[name="pessoa.nome"]').should('be.visible').type('Carol')
+    cy.get('input[name="pessoa.telefonePrincipal"]').should('not.be.disabled').type('61944237856')
+    cy.get('input[name="pessoa.emailPrincipal"]').should('not.be.disabled').type('carol112@gmail.com')
+  })
+
+})
