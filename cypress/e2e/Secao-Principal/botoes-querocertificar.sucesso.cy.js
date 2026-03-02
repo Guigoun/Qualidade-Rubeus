a80aef2e3e640152d53526838a5a39ee7dcb70db
Guigoun <guilhermelicati2004@gmail.com>
2026-03-02 14:23:21 -0300

    Refatorando script de validação do redirecionamento do botão 'Quero me certificar'

---
+++ cypress/e2e/Secao-Principal/botoes-querocertificar.sucesso.cy.js
@@ -0,0 +1,36 @@
+describe("Botões 'Quero me certificar'", () => {
+  beforeEach(() => {
+
+    //Trata o erro no domínio da Rubeus
+    cy.on("uncaught:exception", (err) => {
+      if (err.message.includes("jetStickySection")) {
+        return false;
+      }
+    });
+    
+    //Acessa o site a ser testado
+    cy.visit("https://qualidade.apprbs.com.br/certificacao");
+  });
+
+  it("Deve validar o redirecionamento para página da rubeus", () => {
+    //Clica no botão e permite ficar na mesma aba
+    cy.get("#ivw5ng")
+      .invoke("removeAttr", "target")
+      .should("be.visible")
+      .and("have.attr", "href", "https://rubeus.com.br/")
+      .click({ force: false });
+  });
+
+  it("Deve validar o redirecionamento para página da rubeus", () => {
+
+    //Acessa o site novamente - Evita que fique preso na tela branca do domínio externo
+    //A automação está sofrendo conflito de domínio
+    cy.visit("https://qualidade.apprbs.com.br/certificacao");
+    // Busca pelo link que contém o texto e o destino correto
+    cy.get("#i72bga")
+      .invoke("removeAttr", "target")
+      .should("be.visible")
+      .and("have.attr", "href", "https://rubeus.com.br/")
+      .click({ force: false });
+  });
+});
