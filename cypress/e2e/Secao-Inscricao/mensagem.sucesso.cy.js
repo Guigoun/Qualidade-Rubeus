f9b4a512987876ae556c0f894d4ad12468f72024
Guigoun <guilhermelicati2004@gmail.com>
2026-03-02 11:13:36 -0300

    Correção: implementando cy.wait para estabilizar preenchimento do formulário intermitente

--- cypress/e2e/Secao-Inscricao/mensagem.sucesso.cy.js
+++ cypress/e2e/Secao-Inscricao/mensagem.sucesso.cy.js
@@ -1,26 +1,32 @@
-describe('Preenchimento do formulário de inscrição', () => {
-
+describe("Preenchimento do formulário de inscrição", () => {
   beforeEach(() => {
-    cy.visit('https://qualidade.apprbs.com.br/certificacao')
-  })
+    cy.visit("https://qualidade.apprbs.com.br/certificacao");
+  });
 
-  it('Deve preencher o formulário com nome válido', () => {
-
+  it("Deve preencher o formulário com nome válido", () => {
     // Ignora o erro específico da aplicação
-    cy.on('uncaught:exception', (err) => {
-      if (err.message.includes('ActionsForm')) {
-        return false
+    cy.on("uncaught:exception", (err) => {
+      if (err.message.includes("ActionsForm")) {
+        return false;
       }
-    })
+    });
 
     // Preenche os campos nome, telefone, e-mail
-    cy.get('input[name="pessoa.nome"]').should('be.visible').type('Carol')
-    cy.get('input[name="pessoa.telefonePrincipal"]').should('be.enabled').type('61944237856')
-    cy.get('input[name="pessoa.emailPrincipal"]').should('be.enabled').type('carol112@gmail.com')
-    //Clica no botão 'AVANÇAR'
-    cy.get('#rbBtnNext').click()
-    //Verifica se a menssagem de erro é exibida na tela
-    cy.contains('É necessário informar a base legal').should('be.visible')
-  })
+    cy.get('input[name="pessoa.nome"]')
+    .should("be.visible")
+    .type("Carol");
 
-})
+    cy.wait(1000)
+    cy.get('input[name="pessoa.telefonePrincipal"]')
+      .should("be.enabled")
+      .type("61944237856");
+
+    cy.get('input[name="pessoa.emailPrincipal"]')
+      .should("be.enabled")
+      .type("carol112@gmail.com");
+    //Clica no botão 'AVANÇAR'
+    cy.get("#rbBtnNext").click();
+    //Verifica se a menssagem de erro é exibida na tela
+    cy.contains("É necessário informar a base legal").should("be.visible");
+  });
+});
