433e04ee8f085eb95b94796022e97f5eab4786cb
Guigoun <guilhermelicati2004@gmail.com>
2026-03-02 10:46:39 -0300

    Implementando cy.wait para estabilizar preenchumento de formulário intermitente

--- cypress/e2e/Secao-Inscricao/form.sucesso.cy.js
+++ cypress/e2e/Secao-Inscricao/form.sucesso.cy.js
@@ -1,22 +1,25 @@
-describe('Preenchimento do formulário de inscrição', () => {
-
+describe("Preenchimento do formulário de inscrição", () => {
   beforeEach(() => {
-    cy.visit('https://qualidade.apprbs.com.br/certificacao')
-  })
+    cy.visit("https://qualidade.apprbs.com.br/certificacao");
+  });
 
-  it('Deve preencher o formulário com nome válido', () => {
-
+  it("Deve preencher o formulário com nome válido", () => {
     // Ignora o erro específico da aplicação
-    cy.on('uncaught:exception', (err) => {
-      if (err.message.includes('ActionsForm')) {
-        return false
+    cy.on("uncaught:exception", (err) => {
+      if (err.message.includes("ActionsForm")) {
+        return false;
       }
-    })
+    });
 
     // Preenche o campo Nome
-    cy.get('input[name="pessoa.nome"]').should('be.visible').type('Carol')
-    cy.get('input[name="pessoa.telefonePrincipal"]').should('not.be.disabled').type('61944237856')
-    cy.get('input[name="pessoa.emailPrincipal"]').should('not.be.disabled').type('carol112@gmail.com')
-  })
-
-})
+    cy.get('input[name="pessoa.nome"]').should("be.visible").type("Carol");
+    cy.wait(1000);
+    cy.get('input[name="pessoa.telefonePrincipal"]')
+      .should("not.be.disabled")
+      .type("61944237856");
+      
+    cy.get('input[name="pessoa.emailPrincipal"]')
+      .should("not.be.disabled")
+      .type("carol112@gmail.com");
+  });
+});
